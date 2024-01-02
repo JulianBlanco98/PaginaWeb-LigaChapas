5b98f096199514716f2b344bf2900abc6b738a25
JulianBlanco98 <jblancoho@alumnos.unex.es>
2024-01-03 00:23:59 +0100

    tabla dinamica terminada

--- JS/clasificacionAPI.js
+++ JS/clasificacionAPI.js
@@ -1,13 +1,80 @@
+const apiKey = 'd57c379060684b589b6e000b0ff8bb79'; // apiKey
+const url = 'https://api.football-data.org/v4/competitions/PD/standings';
+
+const headers = {
+    'X-Auth-Token': 'd57c379060684b589b6e000b0ff8bb79',
+};
+
+$.ajax({
+    url: url,
+    method: 'GET',
+    headers: headers,
+    dataType: 'json',
+
+    success: function (result) {
+        console.log(result);
+        rellenarTabla(result);
+    },
+    error: function (error) {
+        console.error(error);
+    }
+});
+
+function rellenarTabla(result) {
+
+    var tbody = document.querySelector('#tabla tbody');
+    var datos = result.standings[0].table;
+    console.log("Numero de equipos: " + datos.length);
+    for (let i = 0; i < datos.length; i++) {
+        var fila = tbody.insertRow(-1); //Agregar las filas al final
+
+        var celdas = ['position', 'team', 'crest', 'points', 'playedGames', 'won', 'draw', 'lost', 'goalsFor', 'goalsAgainst', 'goalDifference'];
+        //console.log("Valores de la fila " + i + ":");
+        for (let j = 0; j < celdas.length; j++) {
+            var celda = fila.insertCell(j);
+
+            if (celdas[j] === 'crest') {
+                var img = document.createElement('img');
+                img.src = datos[i].team[celdas[j]];
+                img.alt = datos[j].team.name;
+                celda.appendChild(img);
+                console.log(celdas[j] + ": " + datos[i].team[celdas[j]]);
+            }
+            else if (celdas[j] === 'team') {
+                celda.textContent = datos[i].team.shortName;
+                console.log(celdas[j] + ": " + datos[i].team.shortName);
+            }
+            else {
+                celda.textContent = datos[i][celdas[j]];
+                console.log(celdas[j] + ": " + datos[i][celdas[j]]);
+            }
+        }
+        if (i < 4) {
+            fila.classList.add('champions');
+        } else if (i === 4) {
+            fila.classList.add('uefa');
+        } else if (i === 5) {
+            fila.classList.add('conference');
+        } else if (i > datos.length - 4) {
+            fila.classList.add('descenso');
+        }
+    }
+}
+
 /*const apiKey = 'd57c379060684b589b6e000b0ff8bb79'; // apiKey
 const url = 'http://api.football-data.org/v4/competitions/PD/standings';
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'd57c379060684b589b6e000b0ff8bb79',
-	}
+    method: 'GET',
+    headers: {
+        'X-Auth-Token': 'd57c379060684b589b6e000b0ff8bb79',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'x-auth-token, x-response-control',
+        'Content-Length': 0,
+        'Content-Type': 'text/plain',
+    }
 };
-
 
 (async () => {
     try {
@@ -19,13 +86,14 @@ const options = {
     }
 })();*/
 
+/*
 const url = 'https://api-football-v1.p.rapidapi.com/v3/timezone';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e84baa8dc6mshb0ad7cf505013e0p12ef72jsna98fcb32705d',
-		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e84baa8dc6mshb0ad7cf505013e0p12ef72jsna98fcb32705d',
+        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+    }
 };
 
 (async () => {
@@ -36,7 +104,7 @@ const options = {
     } catch (error) {
         console.error(error);
     }
-})();
+})();*/
 
 /*const resquest = fetch(url, {
     method: "GET",
